aa432dc5885eb33b047332d4c9167222fe77ef8d
AjithPanneerselvam <ajith.panneerselvam@qubecinema.com>
2018-09-15 20:28:38 +0530

    bootable disk image created; can boot it in a virtual machine or copy it to an USB drive to boot it on real hardware.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
 #![feature(panic_implementation)] 
 #![no_main]
 
+extern crate bootloader_precompiled;
+
 use core::panic::PanicInfo;
 
 /// This function is called on panic 
@@ -13,9 +15,18 @@ pub fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-    loop{}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
